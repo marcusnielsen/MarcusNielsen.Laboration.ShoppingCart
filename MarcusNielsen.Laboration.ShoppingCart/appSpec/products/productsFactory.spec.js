05e06591150702f080ce9d872049196899b25a6a
Marcus Nielsen <marcus.nielsen@r2m.se>
2013-12-02 14:31:01 +0100

    Testing a commit from Git for Windows

---
+++ MarcusNielsen.Laboration.ShoppingCart/appSpec/products/productsFactory.spec.js
@@ -0,0 +1,33 @@
+﻿describe("products factory", function () {
+    var productsFactory;
+    var jsonProductFeedResultMockFactory;
+
+    beforeEach(function () {
+        module('shoppingCartModule');
+
+        module(function ($provide) {
+            $provide.factory('jsonProductFeedResultMockFactory', function () {
+                return {
+                    products: [{
+                        title: 'Duck Tales'
+                    }]
+                };
+            });
+        });
+
+        inject(function (_productsFactory_, _jsonProductFeedResultMockFactory_) {
+            productsFactory = _productsFactory_;
+            jsonProductFeedResultMockFactory = _jsonProductFeedResultMockFactory_;
+        });
+    });
+
+    it('should not be able to reserve product by title when not in database', function () {
+        expect(function () {
+            productsFactory.reserveProductByTitle("Hairy Tails");
+        }).toThrow();
+    });
+
+    it('should be able to reserver product by title', function () {
+        expect(productsFactory.reserveProductByTitle("Duck Tales")).toBe(true);
+    });
+});
