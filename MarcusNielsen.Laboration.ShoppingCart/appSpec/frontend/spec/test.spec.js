764c9ff5d636c1502aed9bcf542fd57df7725b8d
Marcus Nielsen <marcus.nielsen82@gmail.com>
2013-12-12 12:34:12 +0100

    Added a simple protractor test.

---
+++ MarcusNielsen.Laboration.ShoppingCart/appSpec/frontend/spec/test.spec.js
@@ -0,0 +1,12 @@
+describe('Start page', function() {
+  var ptor = protractor.getInstance();
+
+  beforeEach(function() {
+    ptor.get('#/');
+  });
+
+  it('should have a focused product with title Mega Man', function() {
+    var title = ptor.findElement(protractor.by.binding('products.getFocusedProduct().title'));
+    expect(title.getText()).toEqual('Mega Man');
+  });
+});
